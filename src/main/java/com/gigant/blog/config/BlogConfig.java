9cf0e41515938cbdff5b1635d30c121cfbf721e1
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-04-16 11:20:01 +0400

    updated user page

--- src/main/java/com/gigant/blog/config/BlogConfig.java
+++ src/main/java/com/gigant/blog/config/BlogConfig.java
@@ -1,8 +1,14 @@
 package com.gigant.blog.config;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class BlogConfig extends WebMvcConfigurationSupport {
@@ -11,6 +17,15 @@ public class BlogConfig extends WebMvcConfigurationSupport {
     protected void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("welcome");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(){
+        return args -> {
+            Path path = Paths.get("src/main/resources/static/userdata");
+            Files.deleteIfExists(path);
+
+        };
     }
 
 }
